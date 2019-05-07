54a83d5be68aa645e938fa6f3a44c137ab897c63
Rusheng <zrs1990@gmail.com>
2019-05-07 13:23:53 -0400

    add user profile page

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,15 +1,66 @@
 <template>
   <div>
-    <h1>User Profile: 这里放用户的个人信息，目前直接把数据库信息打印出来，需要人合理放置：</h1>
-    {{user_profile}}
+    <v-container class="my-5">
+
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3>
+          <v-card flat class="text-xs-center ma-3" color="grey lighten-3">
+            <v-avatar
+            tile
+            size="100"
+            >
+              <v-img :src="avatar_path"></v-img>
+            </v-avatar>
+            <v-card-text>
+              <p>{{user_profile.username}}</p>
+              <p>{{user_profile.signature}}</p>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+        <p> </p>
+        <v-flex xs12 md6>
+          <div class="caption grey--text">
+            <p>邮箱: {{user_profile.email}}</p>
+          </div>
+        </v-flex>
+        <v-flex xs12 md6>
+          <div class="caption grey--text">
+             <p>账户余额: {{user_profile.balance}}</p>
+          </div>
+        </v-flex>
+        <v-flex xs12 md6>
+          <div class="caption grey--text">
+            <p>生日: {{user_profile.birthday}}</p>
+          </div>
+        </v-flex>
+        <v-flex xs12 md6>
+          <div class="caption grey--text">
+            <p>City:</p>
+          </div>
+        </v-flex>
+        <v-flex xs12 md6>
+          <div class="caption grey--text">
+            <p>签名: {{user_profile.signature}}</p>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 <script>
 import {mapGetters} from 'vuex'
+import {server_ip, axios_config} from "../configs/web_configs"
 export default {
   computed:{
     ...mapGetters({
       user_profile: "user_profile"
-    })}
+    }),
+    avatar_path(){
+      return server_ip+this.user_profile.avatar_path
+    }
+  },
+
 }
 </script>
