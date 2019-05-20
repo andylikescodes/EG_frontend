ad80e4bc49acbb6f776e3b788c556d1e9bec1357
Rusheng <zrs1990@gmail.com>
2019-05-20 01:01:07 -0400

    add pwa config

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// vue.config.js
+module.exports = {
+  // options...
+  pwa: {
+    name: 'EG',
+    themeColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
+}
