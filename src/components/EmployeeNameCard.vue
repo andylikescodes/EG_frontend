699d52556ff64802ee9154bf618cfe67d2925817
Rusheng <zrs1990@gmail.com>
2019-05-23 14:23:25 -0400

    add employee namecard actions

--- src/components/EmployeeNameCard.vue
+++ src/components/EmployeeNameCard.vue
@@ -1,9 +1,10 @@
 <template >
-<v-card :color="bg_color" class="ma-1 pa-3">
+<v-hover>
+<v-card :color="bg_color" class="ma-1 pa-3" slot-scope="{ hover }" :class="`elevation-${hover ? 12 : 2}`">
     <v-layout justify-center wrap>
       <v-flex xs12>
         <v-avatar size="150">
-          <img :src="compute_path(member.avatar_path)" height="300px">
+          <img :src="compute_path(member.avatar_path)" height="200px">
         </v-avatar>
       </v-flex>
       </v-layout>
@@ -19,16 +20,32 @@
         </v-flex>
       </v-layout>
       <v-divider light></v-divider>
-      <v-layout>
-        <v-flex xs2 v-for="service in JSON.parse(member.services)">
+      <v-layout wrap>
+        <v-flex xs4 md2 v-for="service in JSON.parse(member.services)">
           <div class="text-xs-center">
             <v-chip>
+              <!-- <v-avatar v-show="service in icon_paths">
+                <img :src="compute_path(icon_paths[service])">
+              </v-avatar> -->
               {{service}}
             </v-chip>
+              <div >
+                <v-layout row>
+                <v-expand-transition>
+                <v-btn  v-show="hover" color="primary" @click="message" class=caption>  信息<v-icon dark right>message</v-icon></v-btn>
+                </v-expand-transition>
+                <v-expand-transition>
+                <v-btn v-show="hover"  color="success" @click="make_appointment" class=caption>  预约<v-icon dark right>fa-calendar-check</v-icon></v-btn>
+                </v-expand-transition>
+                </v-layout>
+              </div>
+
         </div>
       </v-flex>
       </v-layout>
   </v-card>
+
+  </v-hover>
   </template>
 <script>
   import {server_ip, axios_config} from "../configs/web_configs"
@@ -43,6 +60,12 @@
     methods:{
       compute_path(path){
         return path
+      },
+      message(){
+        EventBus.$emit("danger_alert","该功能尚未开通")
+      },
+      make_appointment(){
+        EventBus.$emit("danger_alert","我们正在加班加点完成该功能，预约请直接联系客服！")
       }
     },
     mounted(){
@@ -51,5 +74,4 @@
   }
 </script>
 <style>
-
 </style>
