a5b90bca86b0b110ae98503b053e8997a5389191
Rusheng <zrs1990@gmail.com>
2019-06-11 19:25:53 -0400

    ckeditor

---
+++ src/views/Edit.vue
@@ -0,0 +1,15 @@
+<template>
+    <ArticleEditor/>
+</template>
+
+<script>
+    import ArticleEditor from '../components/ArticleEditor';
+
+    export default {
+        components: {ArticleEditor},
+        data() {
+            return {
+            };
+        }
+    }
+</script>
